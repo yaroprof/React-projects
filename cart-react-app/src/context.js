ea7a1656fcce056bd07ec9f811442eece1b5dc97
yaroprof <crosskyiv@gmail.com>
2023-04-07 17:31:08 +0300

    add toggleAmount

--- cart-react-app/src/context.js
+++ cart-react-app/src/context.js
@@ -1,4 +1,4 @@
-import React, { useContext, useReducer } from 'react';
+import React, { useContext, useEffect, useReducer } from 'react';
 import cartItems from './data';
 import reducer from './reducer';
 
@@ -9,8 +9,8 @@ const AppContext = React.createContext();
 const initialState = {
   cart: cartItems,
   loading: false,
-  total: 0, 
-  amount: 0
+  total: 0,
+  amount: 0,
   // Add any other properties you need in the initial state
 };
 
@@ -18,29 +18,36 @@ const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const clearCart = (id) => {
-    dispatch({ type: 'CLEAR_CART', payload: id })
-  }
+    dispatch({ type: 'CLEAR_CART', payload: id });
+  };
 
-  const remove = (id) =>{
-    dispatch({ type: 'REMOVE', payload: id })
-  }
+  const remove = (id) => {
+    dispatch({ type: 'REMOVE', payload: id });
+  };
 
   const increase = (id) => {
-    dispatch({ type: 'INCREASE', payload: id })
-  }
+    dispatch({ type: 'INCREASE', payload: id });
+  };
+
+  const toggleAmount = (id, type) => {
+    dispatch({ type: 'TOGGLE_AMOUNT', payload: { id, type } });
+  };
 
 
-  return <AppContext.Provider 
-  value={{
-     ...state,
-     remove,
-     clearCart,
-     increase,
-    //  decrease
-    
-    }}
-  >{children}
-  </AppContext.Provider>;
+  return (
+    <AppContext.Provider
+      value={{
+        ...state,
+        remove,
+        clearCart,
+        increase,
+        toggleAmount,
+
+        //  decrease
+      }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 export const useGlobalContext = () => {
--- cart-react-app/src/reducer.js
+++ cart-react-app/src/reducer.js
@@ -1,23 +1,38 @@
 import CartItem from './components/CartItem';
 
 const reducer = (state, action) => {
-  if(action.type === 'CLEAR_CART'){
-    return{...state, cart: []}
+  if (action.type === 'CLEAR_CART') {
+    return { ...state, cart: [] };
   }
   if (action.type === 'REMOVE') {
     return {
       ...state,
-      cart: state.cart.filter((cartItem) => cartItem.id !== action.payload)
+      cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
     };
   }
+  
   if (action.type === 'INCREASE') {
     let tempCart = state.cart.map((cartItem) => {
-      if (cartItem === action.payload){
-        return{ ...cartItem, amount: cartItem.amount + 1 }
+      if (cartItem === action.payload) {
+        return { ...cartItem, amount: cartItem.amount + 1 };
       }
-      return cartItem
-    })
-    return { ...state, cart: tempCart }
+      return cartItem;
+    });
+    return { ...state, cart: tempCart };
+  }
+
+  if (action.type === 'TOGGLE_AMOUNT') {
+    let tempCart = state.cart
+      .map((cartItem) => {
+        if (cartItem.id === action.payload.id) {
+          if (action.payload.type === 'inc') {
+            return { ...cartItem, amount: cartItem.amount + 1 };
+          }
+        }
+        return cartItem;
+      })
+      .filter((cartItem) => cartItem.amount !== 0);
+    return { ...state, cart: tempCart };
   }
 };
 export default reducer;
