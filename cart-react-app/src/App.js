6e3fad56feda9fac304562aef308fe08002e200f
yaroprof <crosskyiv@gmail.com>
2023-04-10 17:16:52 +0300

    final commit

--- cart-react-app/src/App.js
+++ cart-react-app/src/App.js
@@ -3,23 +3,20 @@ import React from 'react';
 
 import Navbar from './components/Navbar';
 import CartContainer from './components/CartContainer';
-// import { AppProvider } from './context';
+import { AppProvider } from './context';
 
 function App() {
-  // const { loading } = useGlobalContext();
-  // if (loading) {
-  //   return (
-  //     <div className="loading">
-  //       <h1>Loading...</h1>
-  //     </div>
-  //   );
-  // }
 
   return (
-    <main>
-      <Navbar />
-      <CartContainer />
-    </main>
+    <div className="App">
+
+      <main>
+        <AppProvider>
+          <Navbar />
+          <CartContainer />
+        </AppProvider>
+      </main>
+    </div>
   );
 }
 
