7601f430da834d789a67c42cc332bef5ba4e683d
yaroprof <crosskyiv@gmail.com>
2023-04-19 16:09:06 +0300

    nnm

---
+++ 03-stripe-submenus-app/src/App.js
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Submenu from "./components/Submenu"
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <Submenu />
+    </div>
+  );
+}
+
+export default App;
