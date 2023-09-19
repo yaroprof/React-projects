e412b44e0176d910d257358ddd08d4dee16a569f
yaroprof <crosskyiv@gmail.com>
2023-09-19 21:27:38 +0300

    nnm

--- 06-react-menu/src/Menu.js
+++ 06-react-menu/src/Menu.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const Menu = ({ items }) => {
+  
+  return (
+    <div className='section-center'>
+      {items.map((menuItem) => {
+        const { id, title, img, desc, price } = menuItem
+        return (
+          <article className='menu-item' key={id}>
+            <img className='photo' src={img} alt="title" />
+            <div className='item-info'>
+              <header>
+                <h4>{title}</h4>
+                <h4 className='price'>${price}</h4>
+              </header>
+              <p className='item-text'>{ desc}</p>
+            </div>
+          </article>
+        )
+      })}
+    </div>
+
+
+
+  )
 };
 
 export default Menu;
