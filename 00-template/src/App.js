d37ea630bcb929071db42460ef2a740a70b06c26
yaroprof <crosskyiv@gmail.com>
2023-09-26 20:54:36 +0300

    nnm

--- 00-template/src/App.js
+++ 00-template/src/App.js
@@ -1,10 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Error from './pages/Error';
 
 function App() {
   return (
-    <div className="App">
-      TEST
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
   );
 }
 
