7a2ebe973d96d9d344718849f12d66400ed70e99
yaroprof <crosskyiv@gmail.com>
2023-09-27 23:23:12 +0300

    initial commit

---
+++ 09-store/src/modules/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Hero from '../../components/Hero';
+import Products from '../../components/Products';
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <Products />
+    </>
+  );
+};
+
+export default Home;
