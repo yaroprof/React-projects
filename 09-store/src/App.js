e4c7dbe97f65c2a2975d1e660d9587281e98c6a9
yaroprof <crosskyiv@gmail.com>
2023-10-05 16:07:19 +0300

    nnm

--- 09-store/src/App.js
+++ 09-store/src/App.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './modules/Home';
-
+import Product from './modules/Product';
+import ProductCard from './components/ProductCard';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header />
-      <Home />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:id" element={<Product />} />
+        <Route path="/productCard" element={<ProductCard />}   />
+      </Routes>
+
+      <Footer />
     </div>
   );
 }
---
+++ 09-store/src/modules/Products/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// import { Routes, Route } from 'react-router-dom';
+import ProductCard from '../../components/ProductCard'
+import FeatureCard from '../../components/FeatureCard'
+
+const Products = () => {
+  return (
+    <div>
+      <FeatureCard />
+      <ProductCard />
+    </div>
+  )
+}
+
+export default Products  
